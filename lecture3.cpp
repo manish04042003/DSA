7be6f227f5e77473d992a52828ce238284c641ea
manish04042003 <manishkumar817835@gmail.com>
2023-07-11 23:29:51 +0530

    lecture 3

---
+++ lecture3.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a= 3;
+    int b = 5;
+    if (a>b)
+    {
+        cout << "a is greater than b " << endl;
+    }else{
+        cout << "b is greater than a" << endl;
+    }
+    
+
+}
+
