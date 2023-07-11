a4783db93d4b911e67f12c1cf966c247f2429ed3
manish04042003 <manishkumar817835@gmail.com>
2023-07-11 23:54:42 +0530

    staring with loop

--- lecture3.cpp
+++ lecture3.cpp
@@ -1,16 +1,54 @@
 #include <iostream>
 using namespace std;
 
-int main(){
-    int a= 3;
-    int b = 5;
-    if (a>b)
-    {
-        cout << "a is greater than b " << endl;
-    }else{
-        cout << "b is greater than a" << endl;
-    }
+// int main(){
+//     int a;
+//     cin>>a;
+//     int b ;
+//     cin>>b;
+//     if (a>b)
+//     {
+//         cout << "a is greater than b " << endl;
+//     }else{
+//         cout << "b is greater than a" << endl;
+//     }
     
 
-}
+// }
 
+
+
+
+// int main(){
+//     char a;
+//     cin>>a;
+//     if (a>'a' && a<'z')
+//     {
+//         cout << "a---z" << endl;
+//     }else if(a>'A' && a<'Z'){
+//         cout << "A---Z" << endl;
+//     }else if (a>'0' && a<'9')
+//     {
+//         cout << "0---9" << endl;
+//     }
+
+// }
+
+
+
+
+
+int main(){
+    char a;
+    cin>>a;
+    if (a>'a' && a<'z')
+    {
+        cout << "a---z" << endl;
+    }else if(a>'A' && a<'Z'){
+        cout << "A---Z" << endl;
+    }else if (a>'0' && a<'9')
+    {
+        cout << "0---9" << endl;
+    }
+
+}
